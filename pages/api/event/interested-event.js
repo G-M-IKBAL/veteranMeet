8116226b5c1344af77082b0ee67890447b429edb
Talha <talhanasr3e@gmail.com>
2022-12-07 10:02:16 +0500

    added org-routes and  updated and tested the profile schema

---
+++ pages/api/event/interested-event.js
@@ -0,0 +1,35 @@
+import connectMongo from '../../../database/conn';
+import Users from "../../../model/Schema";
+
+
+export default async function handler(req, res){
+    connectMongo().catch(error => res.json({ error: "Connection Failed...!"}))
+
+    // only post method is accepted
+    if(req.method === 'POST'){
+
+        if(!req.body) return res.status(404).json({ error: "Don't have form data...!"});
+
+        await Users.findOneAndUpdate({ email: req.body.email },{ $push: { intrestedEvents: req.body.eventID } })
+        
+        res.status(201).json({ status : true, results: req.body})
+
+
+    } else{
+        res.status(500).json({ message: "HTTP method not valid only POST Accepted"})
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
