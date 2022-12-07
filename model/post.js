546f3f8b07c24c5f3e799a70a3ce6f07780653a0
Talha <talhanasr3e@gmail.com>
2022-12-08 00:56:40 +0500

    added upload file functionality

--- model/post.js
+++ model/post.js
@@ -9,10 +9,10 @@ const postSchema = new Schema({
         type: String,
         required: true
     },
-    img:
+    multimedia:
     {
-        data: Buffer,
-        contentType: String
+        
+        type: String
     }
 
 })
