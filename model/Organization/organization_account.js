3c9b66b01792f9964c5e730643224841377cdd68
Talha <talhanasr3e@gmail.com>
2022-12-07 08:58:16 +0500

    added org schemas

---
+++ model/Organization/organization_account.js
@@ -0,0 +1,17 @@
+import { Schema, model, models } from 'mongoose';
+
+const organizationAccountSchema = new Schema({
+    email: { type: String },
+    password: { type: String },
+    name_of_org: { type: String },
+    category: { type: String },
+    contact: { type: String },
+    city: { type: String }
+})
+
+
+// const Events = models.organization_account || model('Organization_account', Organization_account);
+
+const Organization_account = model('Organization_account', organizationAccountSchema);
+
+export default Organization_account;
