7741f3098eca9ca64cba17cb3e0cd35891b8acfd
Talha <talhanasr3e@gmail.com>
2022-12-08 00:31:09 +0500

    added create post updated route

--- pages/api/post/create-post.js
+++ pages/api/post/create-post.js
@@ -8,16 +8,22 @@ export default function handler(req, res){
     if(req.method === 'POST'){
 
         if(!req.body) return res.status(404).json({ error: "Don't have form data...!"});
-        const { email,text, multimedia} = req.body;
+        const { email,text} = req.body;
        
-
-        Posts.create({
-            email: email,
-            text: text,
-            multimedia: multimedia
-        }, function(err, data){
-            if(err) return res.status(404).json({ err });
-            res.status(201).json({ status : true, post: data})
+        const file = req.files.file;
+        const uploadId = `${Math.random().toString(36)}${Math.random().toString(36)}`;
+        const path = `./uploads/${uploadId}.${file.name.split(".")[1]}`;
+        file.mv(path, (err) => {
+            if (err) {
+                return next(new errorHandler(400, "Error saving file", err));
+            }
+            Posts.create({
+                email: email,
+                text: text,
+                multimedia: path
+            })
+    
+            res.status(200).json({ path: uploadId });
         })
         
        
