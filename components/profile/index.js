32d5c479170c2a21f0207c1f7b831cd2ce3c0e38
Talha <talhanasr3e@gmail.com>
2022-12-12 09:39:10 +0500

    added org info section

--- components/profile/index.js
+++ components/profile/index.js
@@ -4,6 +4,8 @@ function Profile({email}){
 
     const[info,setInfo]= useState({});
     const[name,setName] =useState("");
+
+    const [checkAccount ,setCheckAccount] = useState("");
     
     const[contact,setContact] =useState("");
     const[active_status,setActiveStatus] =useState("false");
@@ -22,8 +24,11 @@ function Profile({email}){
 
     async function handle(){
 
-        
-        await fetch(`http://localhost:3000/api/about/user/${email}`)
+        const checkAcc = email.split('@');
+        setCheckAccount(checkAcc);
+        if(checkAcc[1]!=="org.com")
+        {
+            await fetch(`http://localhost:3000/api/about/user/${email}`)
             .then(res => res.json())
             .then((data) => {
                 if(data) {
@@ -38,6 +43,32 @@ function Profile({email}){
                 }
                 
             })
+
+        }
+        else{
+            ////organization info
+
+            await fetch(`http://localhost:3000/api/about/organization/${email}`)
+            .then(res => res.json())
+            .then((data) => {
+                if(data) {
+  
+                  console.log(data);
+                  setInfo(data.result)
+                  //setActiveStatus("true"); 
+                 
+                }
+                else{
+                  console.log("no data");
+                }
+                
+            })
+
+        }
+        
+        
+
+            
       }
 
 
@@ -51,15 +82,27 @@ function Profile({email}){
                     <h4 className="text-lg font-semibold text-center md:text-left">About</h4>
                     {
                         info===null
-                        ?<div>No information</div>
+                        ?(<div>No information</div>)
                         :
-                        <div>
-                            <p className="dark:text-gray-400">Name:{info.name}</p>
-                            <p className="dark:text-gray-400">Profession:{info.profession}</p>
-                            <p className="dark:text-gray-400">City:{info.city}</p>
-                            <p className="dark:text-gray-400">contact:{info.contact}</p>
-                            <p className="dark:text-gray-400">active :{info.active_status}</p>
-                        </div>    
+                        (
+                            checkAccount[1]!=="org.com"?
+                            <div>
+                                <p className="dark:text-gray-400">Name:{info.name}</p>
+                                <p className="dark:text-gray-400">Profession:{info.profession}</p>
+                                <p className="dark:text-gray-400">City:{info.city}</p>
+                                <p className="dark:text-gray-400">contact:{info.contact}</p>
+                                <p className="dark:text-gray-400">active :{info.active_status}</p>
+                            </div>
+                            :
+                            <div>
+                                <p className="dark:text-gray-400">Name:{info.name_of_org}</p>
+                                <p className="dark:text-gray-400">Category:{info.category}</p>
+                                <p className="dark:text-gray-400">City:{info.city}</p>
+                                <p className="dark:text-gray-400">contact:{info.contact}</p>
+                                
+                            </div>
+                        )    
+
                     }
                     
                     
